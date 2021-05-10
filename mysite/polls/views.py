f1f26e8b56b8464a1bf7942f712ebcc34d145c30
Abdurahmon777 <viktorsara55@gmail.com>
2021-05-10 20:27:15 +0500

    osha django

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,8 +2,35 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Salom")
+# def index(request):
+#     return HttpResponse("Salom")
+#
+# def second(request):
+#     return HttpResponse("Salom men second")
 
-def second(request):
-    return HttpResponse("Salom men second")
+
+from django.shortcuts import render, HttpResponse
+from .models import Question, Choice
+from django.template import loader
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}s.")
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
